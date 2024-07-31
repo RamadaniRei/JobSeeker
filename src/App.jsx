e7b0b8662f638a72f719d0840b96b5fe7a3f36b1
Rei Ramadani <ramadanirei@gmail.com>
2024-07-31 11:41:35 +0200

    Add Starter Files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+    </>
+  );
+};
+
+export default App;
