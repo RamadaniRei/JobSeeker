29e6e97e12aa5abdb53a9b7556d238b7282bda00
Rei Ramadani <ramadanirei@gmail.com>
2024-07-31 14:28:34 +0200

    Adding routers and adding error page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import NavBar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+import "../../src/index.css";
+
 const MainLayout = () => {
   return (
     <>
