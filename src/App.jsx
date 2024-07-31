8ea4bb2c97003589d85a451593b8141db3b58fb5
Rei Ramadani <ramadanirei@gmail.com>
2024-07-31 17:12:24 +0200

    Single Job Page creation

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import HomePage from "../pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "../pages/JobsPage";
 import NotFoundPage from "../pages/NotFoundPage";
-import JobPage from "../pages/JobPage";
+import JobPage, { JobLoader } from "../pages/JobPage";
 import "./index.css";
 
 const router = createBrowserRouter(
@@ -16,8 +16,8 @@ const router = createBrowserRouter(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={JobLoader} />
       <Route path="*" element={<NotFoundPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} />
     </Route>
   )
 );
